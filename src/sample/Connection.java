80d9c070688a18ff04a3506fbfcca986b3de4f19
RonnyFalconeri <ronny@falconeri.de>
2018-01-10 15:26:03 +0100

    Anpassung an Bibliothek.

--- src/sample/Connection.java
+++ src/sample/Connection.java
@@ -3,20 +3,18 @@ package sample;
 import com.aldebaran.qi.Application;
 
 public class Connection {
-    private String IP_Adress="192.168.1.133:9559";
-    private String Port="9559";
-    //private String ConnectionURL=IP_Adress+":"+Port;
+    private String IP_Adress="\"\"tcp://192.168.1.133:9559";
 
     public Application app;
 
     public Connection(){
-        System.out.println("new Classes.Connection()...");
+        System.out.println("new Connection()...");
 
         buildNewConnection();
     }
 
     private void buildNewConnection(){
-        System.out.println("building new connection with IP: '"+this.IP_Adress+":"+this.Port+"' ");
+        System.out.println("building new connection with IP: '"+this.IP_Adress);
 
         // TODO: build a new connection to NAO using the given IP adress and port
         String[] bugfix = new String[]{};
