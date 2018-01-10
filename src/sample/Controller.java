4866c7756a6041be238642cec33f0c0140814b08
RonnyFalconeri <ronny@falconeri.de>
2018-01-10 13:28:31 +0100

    Test Gui überarbeitet

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -5,6 +5,10 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.input.InputMethodEvent;
+import javafx.scene.input.KeyEvent;
+
 
 public class Controller {
     @FXML
@@ -13,9 +17,20 @@ public class Controller {
     @FXML
     Label myLabel;
 
+    @FXML
+    TextField myTextfield;
+
+    Speech speech = new Speech();
+
     public void clickButton(ActionEvent actionEvent) {
         System.out.println("Button wurde geklickt!");
+        speech.sayText();
+    }
 
-        myLabel.setText("Label wurde geändert.");
+
+    public void textChanged(KeyEvent keyEvent) {
+        String Inputtext = myTextfield.getText();
+        System.out.println("set speechtext to: "+myTextfield.getText());
+        speech.setSpeechtext(myTextfield.getText());
     }
 }
