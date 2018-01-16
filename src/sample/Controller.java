97bacde20b8e0abe4cb5bfc5a2f3f133e1fb64a6
RonnyFalconeri <ronny@falconeri.de>
2018-01-16 21:16:28 +0100

    Klassen und GUI überarbeitet.

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -17,54 +17,68 @@ public class Controller {
         @FXML Button ConnectionButton;
         @FXML Circle ConnectionStatus;
 
-
         // Speech
         @FXML TextField SpeechText;
         @FXML Button SpeechButton;
         @FXML ComboBox SpeechLanguage;
         @FXML Slider SpeechPitch;
 
+        // Posture
+        @FXML ComboBox PostureComboBox;
+        @FXML Button PostureButton;
+
     // instanciate new objects here
     Connection connection = new Connection();
     Speech speech = new Speech(connection);
+    Posture posture = new Posture();
 
 
     // methods for GUI actions here
 
-    // Connection
-    public void changeIP_Adress(KeyEvent keyEvent) {
-        connection.setIP_Adress(ConnectionIP.getText());
-    }
+        // Connection
+        public void changeIP_Adress(KeyEvent keyEvent) {
+            connection.setIP_Adress(ConnectionIP.getText());
+        }
 
-    public void changePort(KeyEvent keyEvent) {
-        connection.setPort(ConnectionPort.getText());
-    }
+        public void changePort(KeyEvent keyEvent) {
+            connection.setPort(ConnectionPort.getText());
+        }
 
-    public void clickConnectionButton(ActionEvent actionEvent) {
-        connection.buildNewConnection();
+        public void clickConnectionButton(ActionEvent actionEvent) {
+            connection.buildNewConnection();
 
-    }
+        }
 
-    private void changeConnectionStatus(){
-        // TODO: change color of the connection state dipentend on the actual state
-    }
+        private void changeConnectionStatus(){
+            // TODO: change color of the connection state dipentend on the actual state
+        }
 
 
 
-    // Speech
-    public void changeSpeechText(KeyEvent keyEvent) {
-        speech.setSpeechtext(SpeechText.getText());
-    }
+        // Speech
+        public void changeSpeechText(KeyEvent keyEvent) {
+            speech.setSpeechtext(SpeechText.getText());
+        }
 
-    public void clickSpeechButton(ActionEvent actionEvent) throws Exception {
-        speech.sayText();
-    }
+        public void clickSpeechButton(ActionEvent actionEvent) throws Exception {
+            speech.sayText();
+        }
 
-    public void changeLanguage(ActionEvent actionEvent) {
-        speech.setLanguage(SpeechLanguage.getSelectionModel().getSelectedItem().toString());
-    }
+        public void changeLanguage(ActionEvent actionEvent) {
+            speech.setLanguage(SpeechLanguage.getSelectionModel().getSelectedItem().toString());
+        }
 
-    public void changePitch(MouseEvent mouseEvent) {
-        speech.setPitch(SpeechPitch.getValue());
-    }
+        public void changePitch(MouseEvent mouseEvent) {
+            speech.setPitch(SpeechPitch.getValue());
+        }
+
+
+        // Posture
+        public void changePosture(ActionEvent actionEvent) {
+            posture.setPosture(PostureComboBox.getSelectionModel().getSelectedItem().toString());
+        }
+
+        public void clickPostureButton(ActionEvent actionEvent) {
+            posture.posePosture();
+        }
 }
--- src/sample/Posture.java
+++ src/sample/Posture.java
@@ -1,5 +1,24 @@
 package sample;
 
 public class Posture {
+
     private String Posture;
+
+    public Posture(){
+        System.out.println("new Posture()...");
+
+    }
+
+    // methods for NAO
+    public void posePosture(){
+        System.out.println("posing to: "+this.Posture);
+
+        // TODO: make the NAO pose the given posture
+    }
+
+    // set n' get
+    public void setPosture(String Posture){
+        System.out.println("set Posture to: "+Posture);
+        this.Posture = Posture;
+    }
 }
