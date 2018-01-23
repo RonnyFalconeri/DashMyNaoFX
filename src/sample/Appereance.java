9b9531df5318e6360150bc3b8640229ecc1f393e
Marcel <marcel@marcel.de>
2018-01-23 11:41:14 +0100

    leds

--- src/sample/Appereance.java
+++ src/sample/Appereance.java
@@ -42,4 +42,20 @@ public class Appereance {
     public void setEar_Right_LED(int color){
         this.Ear_Right_LED = color;
     }
+
+////////////////////////////////////////////////////////////////////////
+    // public static ALLeds leds;
+    //leds
+    //public static String name = "AllLeds";
+    //public static String colorname = "red";
+    //public static float duration = 5.0f;
+
+
+    //"white", "red", "green", "blue", "yellow", "magenta", "cyan"
+    //[AllLeds, AllLedsBlue, AllLedsGreen, AllLedsRed, BrainLeds, BrainLedsBack, BrainLedsFront, BrainLedsLeft, BrainLedsMiddle, BrainLedsRight, ChestLeds, EarLeds, FaceLed0, FaceLed1, FaceLed2, FaceLed3, FaceLed4, FaceLed5, FaceLed6, FaceLed7, FaceLedLeft0, FaceLedLeft1, FaceLedLeft2, FaceLedLeft3, FaceLedLeft4, FaceLedLeft5, FaceLedLeft6, FaceLedLeft7, FaceLedRight0, FaceLedRight1, FaceLedRight2, FaceLedRight3, FaceLedRight4, FaceLedRight5, FaceLedRight6, FaceLedRight7, FaceLeds, FaceLedsBottom, FaceLedsExternal, FaceLedsInternal, FaceLedsLeftBottom, FaceLedsLeftExternal, FaceLedsLeftInternal, FaceLedsLeftTop, FaceLedsRightBottom, FaceLedsRightExternal, FaceLedsRightInternal, FaceLedsRightTop, FaceLedsTop, FeetLeds, LeftEarLeds, LeftEarLedsBack, LeftEarLedsEven, LeftEarLedsFront, LeftEarLedsOdd, LeftEye, LeftFaceLeds, LeftFaceLedsBlue, LeftFaceLedsGreen, LeftFaceLedsRed, LeftFootLeds, RightEarLeds, RightEarLedsBack, RightEarLedsEven, RightEarLedsFront, RightEarLedsOdd, RightEye, RightFaceLeds, RightFaceLedsBlue, RightFaceLedsGreen, RightFaceLedsRed, RightFootLeds
+
+    //leds = new ALLeds(app.session());
+    //System.out.println(leds.listGroups());
+    //leds.reset("AllLeds");
+    //leds.fadeRGB(name, colorname, duration);
 }
