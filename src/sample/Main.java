e8ccb6ac7fe70cd946455e97826781896cce089b
Niclas <nici52707qgmx.net>
2018-01-31 14:47:40 +0100

    Hintergrundbild und Icon eingebaut Größe und Anordnung der Bedienelemente angepasst

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -4,7 +4,10 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
+
+import static com.sun.org.apache.xerces.internal.utils.SecuritySupport.getResourceAsStream;
 
 public class Main extends Application {
 
@@ -12,8 +15,11 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         primaryStage.setTitle("Dash My Nao FX");
-        primaryStage.setScene(new Scene(root, 1000, 900));
+        primaryStage.setScene(new Scene(root));
+        primaryStage.setResizable(false);
+        primaryStage.getIcons().add(new Image(Main.class.getResourceAsStream( "icon.png" )));
         primaryStage.show();
+
     }
 
     public static void main(String[] args) { launch(args); }
