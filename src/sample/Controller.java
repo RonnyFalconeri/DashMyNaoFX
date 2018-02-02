75cbdfbadffcbc3274a5164808120366497f9965
RonnyFalconeri <ronny@falconeri.de>
2018-02-02 11:35:34 +0100

    Connection überarbeitet.

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -94,25 +94,36 @@ public class Controller {
 
 
         // Connection
+        int amountClicked=0;
         public void clickConnectionButton(ActionEvent actionEvent) throws Exception {
-
-            if (connection.isConnected()) {
-                // kill connection
-                connection.killConnection();
-
-                // GUI modification
-                changeConnectionState(false);
-                changeButtonText(ConnectionButton, "Connect");
-            } else {
-                // build connection
-                connection = new Connection();
-                connection.setIP_Adress(ConnectionIP.getText());
-                connection.setPort(ConnectionPort.getText());
+            amountClicked++;
+            if (amountClicked < 2) {
+                // connect for the first time
                 connection.buildNewConnection();
 
                 // GUI modification
                 changeConnectionState(true);
                 changeButtonText(ConnectionButton, "Disconnect");
+            } else {
+                // was connected at least once
+                if (connection.isConnected()) {
+                    // kill connection
+                    connection.killConnection();
+
+                    // GUI modification
+                    changeConnectionState(false);
+                    changeButtonText(ConnectionButton, "Connect");
+                } else {
+                    // build connection
+                    connection = new Connection();
+                    connection.setIP_Adress(ConnectionIP.getText());
+                    connection.setPort(ConnectionPort.getText());
+                    connection.buildNewConnection();
+
+                    // GUI modification
+                    changeConnectionState(true);
+                    changeButtonText(ConnectionButton, "Disconnect");
+                }
             }
 
         }
