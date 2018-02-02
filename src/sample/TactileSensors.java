7f5f1c6de8a58bb6e6175ba2528ff28b0adb1066
RonnyFalconeri <ronny@falconeri.de>
2018-02-02 11:38:48 +0100

    Connection überarbeitung.

---
+++ src/sample/TactileSensors.java
@@ -0,0 +1,13 @@
+package sample;
+
+public class TactileSensors {
+
+    private Connection Con;
+
+    public TactileSensors(Connection pCon){
+        System.out.println("new TactileSensors()...");
+        this.Con = pCon;
+    }
+
+
+}
