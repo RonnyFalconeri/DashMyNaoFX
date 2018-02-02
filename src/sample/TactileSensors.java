436a19630f3f9eb30f3d8f3bc1c02a7e9f74c352
marcel.uthe <marcel-uthe@gmx.de>
2018-02-02 11:57:20 +0100

    tactilsensors

--- src/sample/TactileSensors.java
+++ src/sample/TactileSensors.java
@@ -1,5 +1,5 @@
 package sample;
-
+import com.aldebaran.qi.helper.proxies.*;
 public class TactileSensors {
 
     private Connection Con;
@@ -11,3 +11,44 @@ public class TactileSensors {
 
 
 }
+//          public static ALSensors sensors;
+//          public static long frontTactilSubscriptionId;
+//           memory = new ALMemory(app.session());
+//            tts = new ALTextToSpeech(app.session());
+//            frontTactilSubscriptionId = 0;
+//        x = true;
+//          while ( x == true ) {
+//                frontTactilSubscriptionId = memory.subscribeToEvent(
+//                        "FrontTactilTouched", new EventCallback<Float>() {
+//                            @Override
+//                            public void onEvent(Float para)   throws InterruptedException, CallError {
+//                                // 1 means the sensor has been pressed
+//                                if (para > 0) {
+//                                   if (frontTactilSubscriptionId > 0) {
+//                                       frontTactilSubscriptionId = 0;
+//                                        System.out.println("vorne");
+//                                        tts.say("middle");
+//                                    }
+//                                }
+//                            }
+//
+//                        });
+//                memory.subscribeToEvent("MiddleTactilTouched",
+//                        new EventCallback<Float>() {
+//                            @Override
+//                            public void onEvent(Float arg0)
+//                                    throws InterruptedException, CallError {
+//                                if (arg0 > 0) {
+//                                    if (frontTactilSubscriptionId > 0) {
+//                                        System.out.println("Mittel");
+//                                        frontTactilSubscriptionId = 0;
+//                                        x = false;
+//                                        tts.say("Mittel");
+//                                        memory.unsubscribeToEvent(frontTactilSubscriptionId);
+//                                    }
+//                                }
+//                            }
+//
+//                            });
+//
+//           }
